759cd990db08467fc5cc3071948aef1f8589fc08
JuanPLoaizaC <juanpablo9730@gmail.com>
2023-09-04 12:22:39 -0500

    Adding a list of emails

--- Frontend/src/components/Emails.vue
+++ Frontend/src/components/Emails.vue
@@ -11,7 +11,7 @@
       <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm font-sans md:italic" placeholder="Search for anything..." type="text" name="search"/>
     </label>
   </div>
-  <div class="flex flex-col" style="margin-top: 1em;">
+  <div class="flex flex-col pr-8 pl-8" style="margin-top: 1em;">
     <div class="overflow-x-auto">
       <div class="p-1.5 w-full inline-block align-middle">
         <div class="overflow-hidden border rounded-lg">
