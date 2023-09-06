ea9bcccc312d27bbdcf771c5040d6053b7e8d4c6
JuanPLoaizaC <juanpablo9730@gmail.com>
2023-09-05 23:27:12 -0500

    Implement the request to communicate zincsearch

--- Backend/pkg/database/zinc-search.go
+++ Backend/pkg/database/zinc-search.go
@@ -2,24 +2,24 @@ package database
 
 import (
 	"bytes"
-	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
-	"os"
 
 	customerror "github.com/JuanPLoaizaC/mail_search_truora/tree/main/Backend/pkg/database/custom-error"
 	"github.com/JuanPLoaizaC/mail_search_truora/tree/main/Backend/pkg/domain"
 )
 
 var headers = map[string]string{
-	"Content-Type":  "application/json",
-	"Authorization": "Basic",
+	"Content-Type": "application/json",
+	// "Authorization": "Basic",
 }
 
 const (
-	defaultZincSearchHost = "http://localhost:4080"
-	indexName             = "enron_emails"
+	defaultZincSearchHost = "http://localhost:5080"
+	organizationName      = "enron"
+	streamName            = "enron_emails"
 )
 
 type ZincSearchClient struct {
@@ -32,17 +32,12 @@ func NewZincsearchClient(client *http.Client) *ZincSearchClient {
 	}
 }
 
-func (zc *ZincSearchClient) IndexEmails(emails interface{}) (*domain.IndexEmailResponse, error) {
+func (zc *ZincSearchClient) IndexEmails(emailsToinsert interface{}) (*domain.IndexEmailResponse, error) {
 	successResponse := &domain.IndexEmailResponse{}
 	errorResponse := &domain.ErrorReponse{}
-	url := "/api/_bulkv2"
+	url := fmt.Sprintf("%s/api/%s/%s/_json", defaultZincSearchHost, organizationName, streamName)
 
-	bodyRequest := domain.IndexEmailsRequest{
-		Index:   indexName,
-		Records: emails,
-	}
-
-	req, err := makeRequest(url, bodyRequest)
+	req, err := makeRequest(url, emailsToinsert)
 	if err != nil {
 		return nil, err
 	}
@@ -60,6 +55,7 @@ func (zc *ZincSearchClient) IndexEmails(emails interface{}) (*domain.IndexEmailR
 		}
 		return nil, &customerror.CustomDbError{ErrorText: errorResponse.ErrorMessage}
 	}
+
 	err = json.NewDecoder(resp.Body).Decode(successResponse)
 	if err != nil {
 		return nil, err
@@ -73,7 +69,7 @@ func (zc *ZincSearchClient) IndexedSearch(bodyrequest domain.IndexedSearchReques
 	return nil, nil
 }
 
-func makeRequest(url string, body domain.IndexEmailsRequest) (*http.Request, error) {
+func makeRequest(url string, body interface{}) (*http.Request, error) {
 	bodyRequest := makeBodyRequest(body)
 
 	req, err := http.NewRequest("POST", url, bodyRequest)
@@ -97,22 +93,21 @@ func makeBodyRequest(bodyRequest interface{}) io.Reader {
 }
 
 func setHeaders(req *http.Request) {
+	user, pass := getAuthentication()
+	req.SetBasicAuth(user, pass)
+
 	for k, v := range headers {
-		if k == "Authorization" {
-			auth := getAuthentication()
-			req.Header.Set(k, v+" "+auth)
-		} else {
-			req.Header.Set(k, v)
-		}
+		req.Header.Set(k, v)
 	}
 }
 
-func getAuthentication() string {
-	user := os.Getenv("ZINC_FIRST_ADMIN_USER")
-	pass := os.Getenv("ZINC_FIRST_ADMIN_PASSWORD")
+func getAuthentication() (string, string) {
+	// user := os.Getenv("ZO_ROOT_USER_EMAIL")
+	// pass := os.Getenv("ZINC_FIRST_ADMIN_PASSWORD")
 
-	if user == "" || pass == "" {
-		panic("ZINC_FIRST_ADMIN_USER and ZINC_FIRST_ADMIN_PASSWORD must be set")
-	}
-	return base64.StdEncoding.EncodeToString([]byte(user + ":" + pass))
+	// if user == "" || pass == "" {
+	// 	panic("ZO_ROOT_USER_EMAIL and  ZO_ROOT_USER_PASSWORD must be set")
+	// }
+	return "juanpablo9730@gmail.com", "Bur11Caldas"
+	// return base64.StdEncoding.EncodeToString([]byte(user + ":" + pass))
 }
