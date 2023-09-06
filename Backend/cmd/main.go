7a7f7b7e453ca2d7a91aa05703e62b10293d5fa8
JuanPLoaizaC <juanpablo9730@gmail.com>
2023-09-05 23:29:02 -0500

    Creating an index menu

--- Backend/cmd/main.go
+++ Backend/cmd/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	indexer "github.com/JuanPLoaizaC/mail_search_truora/tree/main/Backend/pkg/Indexer"
+)
 
 func main() {
-	fmt.Print("Hoslss")
+	var mode int
+
+	fmt.Println("modes: 1 -> api. 2 -> indexer.")
+	fmt.Scanln(&mode)
+
+	if mode == 1 {
+		fmt.Println("TODO")
+	} else if mode == 2 {
+		indexer.NewIndexer().RunIndexer()
+	} else {
+		fmt.Println("Incorrect data, the options are 1 -> api. 2 -> indexer.")
+	}
 }
