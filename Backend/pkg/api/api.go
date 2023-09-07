7a4e894a2bb05c97e06cad3a4a350ce9bf853838
JuanPLoaizaC <juanpablo9730@gmail.com>
2023-09-07 15:28:21 -0500

    Configurating endpoint for searching

---
+++ Backend/pkg/api/api.go
@@ -0,0 +1,51 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/JuanPLoaizaC/mail_search_truora/tree/main/Backend/pkg/database"
+	"github.com/JuanPLoaizaC/mail_search_truora/tree/main/Backend/pkg/handler"
+	"github.com/JuanPLoaizaC/mail_search_truora/tree/main/Backend/pkg/service"
+	"github.com/go-chi/chi/v5"
+)
+
+const (
+	serverPort = ":8000"
+)
+
+type Api struct {
+	Router               *chi.Mux
+	httpClient           *http.Client
+	indexedSearchHandler *handler.IndexedSearchHandler
+}
+
+func NewApi() *Api {
+	server := &Api{}
+	server.configureHttpClient()
+	server.configureDependencies()
+	server.configureRouter()
+
+	return server
+}
+
+func (a *Api) configureHttpClient() {
+	a.httpClient = &http.Client{}
+}
+
+func (a *Api) configureDependencies() {
+	datasourceZincSearch := database.NewZincsearchClient(a.httpClient)
+
+	indexedSearchService := service.NewIndexedSearchService(datasourceZincSearch)
+	indexedSearchHandler := handler.NewIndexedSearchHandler(indexedSearchService)
+	a.indexedSearchHandler = indexedSearchHandler
+}
+
+func (a *Api) Run() {
+	fmt.Println("Application is running on host http://localhost:8000/")
+
+	err := http.ListenAndServe(serverPort, a.Router)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ Backend/pkg/api/router.go
@@ -0,0 +1,40 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"github.com/go-chi/render"
+)
+
+func (a *Api) configureRouter() {
+	a.Router = chi.NewRouter()
+	a.Router.Use(middleware.RequestID)
+	a.Router.Use(middleware.Logger)
+	a.Router.Use(middleware.Recoverer)
+	a.Router.Use(middleware.URLFormat)
+	a.Router.Use(render.SetContentType(render.ContentTypeJSON))
+	a.configureCORS()
+	a.configureRoutes()
+}
+
+func (a *Api) configureCORS() {
+	a.Router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://127.0.0.1:5173"},
+		AllowedMethods:   []string{"GET", "POST", "DELETE"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+}
+
+func (a *Api) configureRoutes() {
+	a.Router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		render.JSON(w, r, map[string]string{"message": "Welcome to the Go and ZinSearch Challenge!"})
+	})
+
+	a.Router.Get("/search", a.indexedSearchHandler.SearchTermInEmails)
+}
