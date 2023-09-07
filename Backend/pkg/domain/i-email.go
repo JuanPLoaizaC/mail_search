9866326092c5aa08725faa86671f8aac5371e084
JuanPLoaizaC <juanpablo9730@gmail.com>
2023-09-07 15:29:20 -0500

    Correcting structs for making zincsearch requests

--- Backend/pkg/domain/i-email.go
+++ Backend/pkg/domain/i-email.go
@@ -19,39 +19,42 @@ type IndexEmailStatusResponse struct {
 
 // IndexedSearchRequest is the request for the IndexedSearch function
 type IndexedSearchRequest struct {
-	SearchType string                    `json:"search_type"`
-	SortFields []string                  `json:"sort_fields"`
-	From       int                       `json:"from"`
-	MaxResults int                       `json:"max_results"`
-	Query      IndexedSearchRequestQuery `json:"query"`
-	// Source     []string                  `json:"_source"`
+	Aggs  struct{}                  `json:"aggs"`
+	Query QueryIndexedSearchRequest `json:"query"`
 }
 
-// IndexedSearchRequestQuery is the query for the IndexedSearch function
-type IndexedSearchRequestQuery struct {
-	Term string `json:"term"`
-	// StartTime string `json:"start_time"`
-	// EndTime   string `json:"end_time"`
+type QueryIndexedSearchRequest struct {
+	// EndTime   int    `json:"end_time"`
+	From int    `json:"from"`
+	Size int    `json:"size"`
+	Sql  string `json:"sql"`
+	// StartTime int    `json:"start_time"`
 }
 
 // IndexedSearchResponse is the response for the IndexedSearch function
 type IndexedSearchResponse struct {
-	Took     float64 `json:"took"`
-	TimedOut bool    `json:"timed_out"`
-	MaxScore float64 `json:"max_score"`
-	Hits     struct {
-		Total struct {
-			Value int `json:"value"`
-		} `json:"total"`
-		Hits []struct {
-			Index     string                 `json:"_index"`
-			Type      string                 `json:"_type"`
-			ID        string                 `json:"_id"`
-			Score     float64                `json:"_score"`
-			Timestamp string                 `json:"@timestamp"`
-			Source    map[string]interface{} `json:"_source"`
-		} `json:"hits"`
-	} `json:"hits"`
+	Took       int `json:"took"`
+	TookDetail struct {
+		Total            int `json:"total"`
+		WaitQueue        int `json:"wait_queue"`
+		ClusterTotal     int `json:"cluster_total"`
+		ClusterWaitQueue int `json:"cluster_wait_queue"`
+	} `json:"took_detail"`
+	Hits     []HitIndexedSearchResponse `json:"hits"`
+	Total    int                        `json:"total"`
+	From     int                        `json:"from"`
+	Size     int                        `json:"size"`
+	ScanSize int                        `json:"scan_size"`
+}
+
+type HitIndexedSearchResponse struct {
+	Timestamp int64  `json:"_timestamp"`
+	Content   string `json:"content"`
+	Date      string `json:"date"`
+	From      string `json:"from"`
+	MessageID string `json:"message_id"`
+	Subject   string `json:"subject"`
+	To        string `json:"to"`
 }
 
 // ErrorReponse is the response of datasource when an error occurs
