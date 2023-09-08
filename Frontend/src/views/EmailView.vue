daa32ca6edb8aa9a20c3d506eef99a84b8a5579c
JuanPLoaizaC <juanpablo9730@gmail.com>
2023-09-08 17:02:52 -0500

    Paginating the table in EmailVue

--- Frontend/src/views/EmailView.vue
+++ Frontend/src/views/EmailView.vue
@@ -1,7 +1,13 @@
 <script setup>
+import { ref } from 'vue';
 import { useEmailStore } from '../stores/emails';
 import { storeToRefs } from 'pinia';
 import { useRouter } from "vue-router";
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/vue/20/solid';
+
+let start = ref(0);
+let end = ref(10);
+let page = ref(1);
 
 const router = useRouter();
 const useEmail = useEmailStore();
@@ -11,6 +17,18 @@ const redirectToContent = (email) => {
   router.push(`mails/${email.message_id}`);
   emailSelected.value = email;
 };
+
+const previous = () => {
+  page.value --;
+  start.value -= 10;
+  end.value -= 10;
+};
+
+const next = () => {
+  page.value ++;
+  start.value += 10;
+  end.value += 10;
+}; 
 </script>
 
 <template>
@@ -48,7 +66,7 @@ const redirectToContent = (email) => {
               </tr>
             </thead>
             <tbody class="divide-y divide-gray-200">
-              <tr class="hover:bg-slate-100"  v-for="email in emailsList" :key="email.message_id" @click="redirectToContent({ ... email })">
+              <tr class="hover:bg-slate-100"  v-for="email in emailsList.slice(start, end)" :key="email.message_id" @click="redirectToContent({ ... email })">
                 <td class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
                   {{ email.date }}
                 </td>
@@ -65,6 +83,40 @@ const redirectToContent = (email) => {
             </tbody>
           </table>
         </div>
+      </div>
+    </div>
+  </div>
+  <div class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6 mt-4" v-if="emailsList.length > 0">
+    <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+      <div>
+        <p class="text-sm text-gray-700">
+          Showing
+          {{ ' ' }}
+          <span class="font-medium">{{ start + 1 }}</span>
+          {{ ' ' }}
+          to
+          {{ ' ' }}
+          <span class="font-medium">{{ end <= emailsList.length ? end : emailsList.length }}</span>
+          {{ ' ' }}
+          of
+          {{ ' ' }}
+          <span class="font-medium">{{ emailsList.length }}</span>
+          {{ ' ' }}
+          results
+        </p>
+      </div>
+      <div>
+        <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
+          <button class="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0" :disabled="page === 1" @click="previous">
+            <span class="sr-only">Previous</span>
+            <ChevronLeftIcon class="h-5 w-5" aria-hidden="true" />
+          </button>
+          <p aria-current="page" class="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{{ page }}</p>
+          <button class="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0" :disabled="(emailsList.length / end) <= 1" @click="next">
+            <span class="sr-only">Next</span>
+            <ChevronRightIcon class="h-5 w-5" aria-hidden="true" />
+          </button>
+        </nav>
       </div>
     </div>
   </div>
