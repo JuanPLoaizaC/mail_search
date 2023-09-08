aa4e382e4a483a8eef45d3bb78948f1b16464949
JuanPLoaizaC <juanpablo9730@gmail.com>
2023-09-08 13:54:52 -0500

    Adding the run api option

--- Backend/cmd/main.go
+++ Backend/cmd/main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"fmt"
 
-	indexer "github.com/JuanPLoaizaC/mail_search_truora/tree/main/Backend/pkg/Indexer"
+	indexer "github.com/JuanPLoaizaC/mail_search/tree/main/Backend/pkg/Indexer"
+	"github.com/JuanPLoaizaC/mail_search/tree/main/Backend/pkg/api"
 )
 
 func main() {
@@ -13,9 +14,9 @@ func main() {
 	fmt.Scanln(&mode)
 
 	if mode == 1 {
-		fmt.Println("TODO")
+		api.NewApi().Run()
 	} else if mode == 2 {
-		indexer.NewIndexer().RunIndexer()
+		indexer.NewIndexer().Run()
 	} else {
 		fmt.Println("Incorrect data, the options are 1 -> api. 2 -> indexer.")
 	}
